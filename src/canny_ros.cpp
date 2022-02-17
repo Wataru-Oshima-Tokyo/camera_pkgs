1b1f784aea1796bb2a784d115f0fe70d99f440e3
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-02-16 20:10:33 -0800

    fixed bugs

--- src/canny_ros.cpp
+++ src/canny_ros.cpp
@@ -1,11 +1,10 @@
  #include <ros/ros.h>
 
  // Include opencv2
- #include <opencv2/imgproc.hpp>
- #include <opencv2/highgui.hpp>
-//  #include <opencv2/core.hpp>
-//  #include "roscpp_tutorials/TwoInts.h"
-//  #include <opencv2/core/types.hpp>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 
  // Include CvBridge, Image Transport, Image msg
  #include <image_transport/image_transport.h>
@@ -14,6 +13,7 @@
  #include "roscpp_tutorials/TwoInts.h"
  #include <vector>
  #include <map>
+// #include <camera_pkg/Camera_CV.h>
  #define IMG_HEIGHT (240)
  #define IMG_WIDTH (320)
  #define rep(i,a,b) for(int i=a;i<b;i++)
@@ -21,15 +21,14 @@
  using namespace std;
  using namespace cv;
 
-
 class CAMERA_CV{
-
-public:
+  public:
     Mat src, src_gray, dst, detected_edges;
     ros::Publisher pub;
     ros::Subscriber sub;
     ros::NodeHandle nh;
     ros::ServiceServer start, stop;
+    int lowThreshold;
     void CannyThreshold(int, void*);
     const std::string OPENCV_WINDOW = "Image window";
     virtual bool clbk_start_service(roscpp_tutorials::TwoInts::Request& req,roscpp_tutorials::TwoInts::Response& res);
@@ -42,19 +41,23 @@ public:
     const std::string SERVICE_STOP = "/canny/stop";
     CAMERA_CV();
     ~CAMERA_CV();
-    bool getRun();
+    bool getRun(); 
+    const int max_lowThreshold = 100;
+    // static constexpr char* window_name = "Edge Map";
+    static void callback(int pos, void *userptr) { // because trace
+        CAMERA_CV *foo = (CAMERA_CV*)userptr; // cast user data back to "this"
+        //foo->CannyThreshold(pos, userptr);
+    }
 private:
     bool RUN = false;
-    int lowThreshold = 6;
-    const int max_lowThreshold = 100;
     const int ratio = 3;
     //set the kernel size 3
     const int kernel_size = 3;
-    const char* window_name = "Edge Map";
 };
 
-CAMERA_CV::CAMERA_CV(){
 
+CAMERA_CV::CAMERA_CV(){
+  lowThreshold = 6;
 };
 CAMERA_CV::~CAMERA_CV(){};
 
@@ -72,7 +75,7 @@ void CAMERA_CV::CannyThreshold(int, void*)
     // //set the dist image all black so that you can put the deteceted edges on the black background
     dst = Scalar::all(0);
     // src.copyTo( dst, detected_edges);
-    imshow( window_name, src);
+    imshow( "Edge Map", detected_edges);
     waitKey(3);
 }
 
@@ -98,7 +101,7 @@ void CAMERA_CV::CannyThreshold(int, void*)
     cv_bridge::CvImagePtr cv_ptr;
     try
     {
-        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR16);
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
     }
     catch (cv_bridge::Exception& e)
     {
@@ -108,7 +111,7 @@ void CAMERA_CV::CannyThreshold(int, void*)
 
     src = cv_ptr->image;
     dst.create(src.size(), src.type());
-    // cvtColor(src, src_gray, COLOR_BGR2GRAY);
+    cvtColor(src, src_gray, COLOR_BGR2GRAY);
     // namedWindow(window_name, WINDOW_AUTOSIZE );
     // CannyThreshold(0, 0);
 
@@ -118,24 +121,6 @@ void CAMERA_CV::CannyThreshold(int, void*)
 
 int main( int argc, char** argv )
 {
-//   CommandLineParser parser( argc, argv, "{@input | ../data/fruits.jpg | input image}" );
-//   src = imread( parser.get<String>( "@input" ), IMREAD_COLOR ); // Load an image
-    
-//     //call back
-//     Mat src = imread("./ppms/frame200.ppm");
-//     imshow("frame100", src);
-//   if( src.empty() )
-//   {
-//     std::cout << "Could not open or find the image!\n" << std::endl;
-//     std::cout << "Usage: " << argv[0] << " <Input image>" << std::endl;
-//     return -1;
-//   }
-
-  /* you can set the value of Threshold for the canny function. 
-    It can be used for ajusting how much we want to detect the edge
-  */
-//   createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold );
-  //call canny Threashold
 
    ros::init(argc, argv, "roscpp_example");
    CAMERA_CV cc;
@@ -148,18 +133,13 @@ int main( int argc, char** argv )
    while(ros::ok()){
       // cout << cc.getRun() << endl;
       if(cc.getRun()){
+        // createTrackbar( "Min Threshold:", "Edge Map", &cc.lowThreshold, cc.max_lowThreshold, cc.callback, (void*)(&cc));
+        createTrackbar( "Min Threshold:", "Edge Map", &cc.lowThreshold, cc.max_lowThreshold);
         cc.CannyThreshold(0, 0);
       }
       ros::spinOnce();
       loop_rate.sleep();
    }
-
-    // ros::spin();
-   // Instantiate the ROS Node Handler as nh
-
-   // Print "Hello ROS!" to the terminal and ROS log file
-
-   // Program succesful
    
   return 0;
 }
