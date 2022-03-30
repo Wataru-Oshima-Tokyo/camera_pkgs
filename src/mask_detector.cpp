fb8b1e8cd3ff475d27363f8c70f2b6732be4f7a0
Wataru <wataruoshima@my.smccd.edu>
2022-03-29 18:32:17 -0700

    added sth

--- src/mask_detector.cpp
+++ src/mask_detector.cpp
@@ -13,6 +13,7 @@
  #include "std_msgs/String.h"
  #include "std_srvs/Empty.h"
  #include <vector>
+//  #include <object/Coordinate.h>
  #include <camera_pkg/Coordinate.h>
  #include <map>
 
@@ -28,18 +29,18 @@
 struct timespec start, stop;
 double fstart, fstop;
 
-class CAMERA_CV{
+class DETECTOBJ{
   public:
+    //variables
     Mat src, src_gray, src_hsv, dst, detected_edges, mask;
     Mat depth;
     ros::Publisher pub;
-    ros::Subscriber image_sub, depth_sub;
+    ros::Subscriber image_sub, depth_sub, darknet_bbox_sub;
     ros::NodeHandle nh;
+    darknet_ros_msgs::Coordinate coordinate;
     ros::ServiceServer imshow_start, imshow_stop;
-    ros::ServiceClient calibration_start, calibration_stop;
     int lowThreshold;
-    // int low_c[3] = {17, 123, 121};
-    // int high_c[3] ={37, 143, 201};
+    darknet_ros_msgs::BoundingBox detected_object;
     int low_c[3] = {0, 0, 0};
     int high_c[3] = {0, 0, 0};
     const int max_c[3] = {179, 255, 255};
@@ -49,34 +50,35 @@ class CAMERA_CV{
     void CannyThreshold(int, void*);
     void MaskThreshold(int, void*);
     void DrawCircle(int, void*);
+    void detect_object(int , void* userdata);
     void mouseEvent(int event, int x, int y, int flags, void* userdata);
     // Mat getDepth();
     const std::string OPENCV_WINDOW = "Image window";
-    virtual bool calibration_start_service(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res);
-    virtual bool calibration_stop_service(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res);
+    virtual bool maskdetect_start_service(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res);
+    virtual bool maskdetect_stop_service(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res);
     virtual void image_callback(const sensor_msgs::ImageConstPtr&);
     virtual void depth_callback(const sensor_msgs::ImageConstPtr&);
+    virtual void bbox_callback(const darknet_ros_msgs::BoundingBoxes&);
     // Topics
     const std::string IMAGE_TOPIC = "/camera/color/image_raw";
     const std::string DEPTH_TOPIC = "/camera/aligned_depth_to_color/image_raw";
+    const std::string BBOX_TOPIC = "/darknet_ros/bounding_boxes";
     // const std::string DEPTH_TOPIC = "/camera/depth/color/image_raw";
-    const std::string PUBLISH_TOPIC = "/camera_pkg/coordinate";
+    const std::string PUBLISH_TOPIC = "/objectdetection/coordinate";
     const std::string IMSHOW_SERVICE_START = "/imshow/start";
     const std::string IMSHOW_SERVICE_STOP = "/imshow/stop";
-    const std::string CALIB_SERVICE_START = "/calibration/start";
-    const std::string CALIB_SERVICE_STOP = "/calibration/stop";
-    CAMERA_CV();
-    ~CAMERA_CV();
+    const std::string MASK_DETECT_SERVICE_START = "/maskdetect/start";
+    const std::string MASK_DETECT_SERVICE_STOP = "/maskdetect/stop";
+
+    DETECTOBJ();
+    ~DETECTOBJ();
     bool getRun(); 
     const int max_lowThreshold = 100;
     const std::string window_name = "Edge Map";
-    // static constexpr char* window_name = "Edge Map";
-    // static void callback(int event, int x, int y, int flags, void* userdata) { // because the mouse call back cannot accept non-static func
-    //     CAMERA_CV *foo = (CAMERA_CV*)userdata; // cast user data back to "this"
-    //     foo->mouseEvent(event, x, y, flags, foo);
-    // }
 private:
     bool RUN = false;
+    double detect_probability =0.0;
+    bool detected=false;
     bool start_call = true;
     bool stop_call = false;
     const int ratio = 3;
@@ -85,31 +87,38 @@ private:
 };
 
 
-CAMERA_CV::CAMERA_CV(){
+DETECTOBJ::DETECTOBJ(){
   lowThreshold = 6;
 };
 
-CAMERA_CV::~CAMERA_CV(){};
+DETECTOBJ::~DETECTOBJ(){};
 
-bool CAMERA_CV::getRun(){
-  std_srvs::Empty _emp;
-  if(RUN && start_call){
-    calibration_start.call(_emp);
-    start_call = false;
-    stop_call = true;
-  }else if(!RUN && stop_call) {
-    calibration_stop.call(_emp);
-    start_call = true;
-    stop_call = false;
-  }
-    
+bool DETECTOBJ::getRun(){    
   return RUN;
 }
 
+void DETECTOBJ::detect_object(int, void* userdata){
+    DETECTOBJ *cc = (DETECTOBJ*)userdata;
+    cv::Point pt1(detected_object.xmin, detected_object.ymin);
+    cv::Point pt2(detected_object.xmax, detected_object.ymax);
+    cv::Point center((detected_object.xmax+detected_object.xmin)/2, (detected_object.ymax+detected_object.ymin)/2);
+    cv::circle(src, center, 5, cv::Scalar(0, 0, 255));
+    cv::putText(src, "Cup", pt2, FONT_HERSHEY_DUPLEX, 1.0, cv::Scalar(255, 185, 0), 2);
+    cv::rectangle(src, pt1, pt2, cv::Scalar(0,255,0));
+    int z = cc->depth.at<uint16_t>(center.y,center.x);
+    cc->coordinate.x = center.x;
+    cc->coordinate.y = center.y;
+    if(cc->coordinate.x !=0 && cc->coordinate.y!=0){
+      cc->coordinate.z = z;
+    }else{
+      cc->coordinate.z =0;
+    }
+    
+    cc->pub.publish(coordinate);
+}
 
 
-
-void CAMERA_CV::DrawCircle(int, void*){
+void DETECTOBJ::DrawCircle(int, void*){
   int x = src.cols, y = src.rows;
   vector<int> x_array = {x/2-70, x/2, x/2+70, x/2+140};
   vector<int> y_array = {y/2-140, y/2-70, y/2, y/2+70};
@@ -130,8 +139,8 @@ void CAMERA_CV::DrawCircle(int, void*){
 
 }
 
-void CAMERA_CV::MaskThreshold(int, void*){
-
+void DETECTOBJ::MaskThreshold(int, void*userdata){
+   DETECTOBJ *cc = (DETECTOBJ*)userdata;
    cv::inRange(src_hsv, cv::Scalar(low_c[0],low_c[1],low_c[1]), cv::Scalar(high_c[0],high_c[1],high_c[2]),mask);
    
    cv::Moments M = cv::moments(mask); // get the center of gravity
@@ -140,42 +149,35 @@ void CAMERA_CV::MaskThreshold(int, void*){
    			int cy = int(M.m01/M.m00); //重心のy座標
       
       cv::circle(src, cv::Point(cx,cy), 5, cv::Scalar(0, 0, 255));
+      int z = cc->depth.at<uint16_t>(cy,cx);
+        cc->coordinate.x = center.x;
+        cc->coordinate.y = center.y;
+        if(cc->coordinate.x !=0 && cc->coordinate.y!=0){
+        cc->coordinate.z = z;
+        }else{
+        cc->coordinate.z =0;
+        }
    }
-  // //  imshow("masked", src);
-  // //  waitKey(3);
+   
+   
 }
 
 
-
-// void CAMERA_CV::CannyThreshold(int, void*)
-// {
-//     // cout << "start canny threashold" <<endl;
-//     // removeing the noise and ap since the kernel size is 3, set the size 3 by 3
-//     blur(mask, detected_edges, Size(3,3) );
-//     // //detecing an edege by Canny with the lowThreashold and maxThreshold which is 3 times thant the lower one.
-//     Canny(detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
-//     // //set the dist image all black so that you can put the deteceted edges on the black background
-//     dst = Scalar::all(0);
-//     // src.copyTo( dst, detected_edges);
-//     imshow( "Edge Map", detected_edges);
-//     waitKey(3);
-// }
-
- bool CAMERA_CV::calibration_start_service(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res){
+ bool DETECTOBJ::maskdetect_start_service(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res){
   //  cout << "start calibration" << endl;
    RUN = true;
    return RUN;
 
  }
 
- bool CAMERA_CV::calibration_stop_service(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res){
+ bool DETECTOBJ::maskdetect_stop_service(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res){
   //  cout << "stop calibration" << endl;
    RUN = false;
    return RUN;
  }
 
 
-void CAMERA_CV::depth_callback(const sensor_msgs::ImageConstPtr& msg){
+void DETECTOBJ::depth_callback(const sensor_msgs::ImageConstPtr& msg){
     std_msgs::Header msg_header = msg->header;
     std::string frame_id = msg_header.frame_id.c_str();
     // ROS_INFO_STREAM("New Image from " << frame_id);
@@ -197,7 +199,7 @@ void CAMERA_CV::depth_callback(const sensor_msgs::ImageConstPtr& msg){
 
 }
 
- void CAMERA_CV::image_callback(const sensor_msgs::ImageConstPtr& msg){
+ void DETECTOBJ::image_callback(const sensor_msgs::ImageConstPtr& msg){
     std_msgs::Header msg_header = msg->header;
     std::string frame_id = msg_header.frame_id.c_str();
     // ROS_INFO_STREAM("New Image from " << frame_id);
@@ -224,81 +226,30 @@ void CAMERA_CV::depth_callback(const sensor_msgs::ImageConstPtr& msg){
  }
 
 
-
-void mouseEvent(int event, int x, int y, int flags, void* userdata)
-{
-     CAMERA_CV *cc = (CAMERA_CV*)userdata;
-    //  ros::Publisher* _pub = cc->pub;
-    //  _cc.pub = _cc.nh.advertise<std_msgs::String>(_cc.PUBLISH_TOPIC, 1000);
-     camera_pkg::Coordinate coordinate;
-    //  Mat* _depth = &depth;
-     std::string temp="";
-     double z=0.0;
-     z = cc->depth.at<uint16_t>((uint16_t)y,(uint16_t)x);
-     Vec3b &color = src_hsv.at<Vec3b>(Point(y,x));
-     cout << color[0] << " "<< color[1] << " " << color[2] <<endl; 
-     if  ( event == EVENT_LBUTTONDOWN )
-     {
-          //I got the erro for getting the belwo one I guess because this function is dervied from the 
-          // z = cc->depth.at<u_int16_t>(x,y);
-          
-          cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ", " << z << ")" << endl;
-          temp = "L";
-          
-          // cout << z << endl;
-           
-     }
-     else if  ( event == EVENT_RBUTTONDOWN )
-     {
-          cout << "Right button of the mouse is clicked - position (" << x << ", " << y << ", " << z << ")" << endl;
-          temp = "R";
-     }
-     else if  ( event == EVENT_MBUTTONDOWN )
-     {
-          cout << "Middle button of the mouse is clicked - position (" << x << ", " << y << ", " << z << ")" << endl;
-          temp = "M";
-     }
-     if(!temp.empty()){
-       if(z>0 && z <1200){
-          coordinate.t = temp;
-          coordinate.x = x;
-          coordinate.y = y;
-          coordinate.z = z;
-          cc->pub.publish(coordinate);
-       }else{
-         cout << "z value is not valid please try again." << endl;
-       }
-
-     }
-
-}
-
-
 int main( int argc, char** argv )
 {
 
-   ros::init(argc, argv, "work_with_camera_start");
-   CAMERA_CV cc;
+   ros::init(argc, argv, "mask_detector");
+   DETECTOBJ cc;
    // Initialize the ROS Node "roscpp_example"
    ros::Rate loop_rate(20);
    
-   cc.image_sub = cc.nh.subscribe(cc.IMAGE_TOPIC, 1000, &CAMERA_CV::image_callback, &cc);
-   cc.depth_sub = cc.nh.subscribe(cc.DEPTH_TOPIC, 1000, &CAMERA_CV::depth_callback, &cc);
-   cc.imshow_start = cc.nh.advertiseService(cc.IMSHOW_SERVICE_START, &CAMERA_CV::calibration_start_service, &cc);
-   cc.imshow_stop = cc.nh.advertiseService(cc.IMSHOW_SERVICE_STOP, &CAMERA_CV::calibration_stop_service, &cc);
-   cc.calibration_start = cc.nh.serviceClient<std_srvs::Empty>(cc.CALIB_SERVICE_START);
-   cc.calibration_stop = cc.nh.serviceClient<std_srvs::Empty>(cc.CALIB_SERVICE_STOP);
-   cc.pub = cc.nh.advertise<camera_pkg::Coordinate>(cc.PUBLISH_TOPIC, 1000);
+   cc.image_sub = cc.nh.subscribe(cc.IMAGE_TOPIC, 1000, &DETECTOBJ::image_callback, &cc);
+   cc.depth_sub = cc.nh.subscribe(cc.DEPTH_TOPIC, 1000, &DETECTOBJ::depth_callback, &cc);
+   cc.darknet_bbox_sub = cc.nh.subscribe(cc.BBOX_TOPIC, 1000, &DETECTOBJ::bbox_callback, &cc);
+   cc.imshow_start = cc.nh.advertiseService(cc.IMSHOW_SERVICE_START, &DETECTOBJ::maskdetect_start_service, &cc);
+   cc.imshow_stop = cc.nh.advertiseService(cc.IMSHOW_SERVICE_STOP, &DETECTOBJ::maskdetect_stop_service, &cc);
+   cc.pub = cc.nh.advertise<darknet_ros_msgs::Coordinate>(cc.PUBLISH_TOPIC, 1000);
+   
    std_srvs::Empty _emp;
    while(ros::ok()){
       // cout << cc.getRun() << endl;
-       clock_gettime(CLOCK_MONOTONIC, &start); fstart=(double)start.tv_sec + ((double)start.tv_nsec/1000000000.0);
-       
+      clock_gettime(CLOCK_MONOTONIC, &start); fstart=(double)start.tv_sec + ((double)start.tv_nsec/1000000000.0);
       if(cc.getRun()){
-            cc.DrawCircle(0,0);
+          cc.MaskThreshold(0,&cc);
       }
       if(!cc.src.empty()){
-        setMouseCallback("src", mouseEvent, &cc);
+        // setMouseCallback("src", mouseEvent, &cc);
         clock_gettime(CLOCK_MONOTONIC, &stop); fstop=(double)stop.tv_sec + ((double)stop.tv_nsec/1000000000.0);
         std::string fps= "FPS: " + std::to_string(1/(fstop-fstart));
         putText(cc.src, //target image
