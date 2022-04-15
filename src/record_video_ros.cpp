db39c65f50c052692002b2a3f5bbb1fca2eeba54
Wataru <wataruoshima@my.smccd.edu>
2022-04-15 16:34:14 -0700

    added

---
+++ src/record_video_ros.cpp
@@ -0,0 +1,66 @@
+  #include <ros/ros.h>
+
+ // Include opencv2
+ #include <opencv2/imgproc/imgproc.hpp>
+ #include <opencv2/highgui/highgui.hpp>
+ #include <opencv2/core/core.hpp>
+//  #include <opencv2/core/types.hpp>
+
+ // Include CvBridge, Image Transport, Image msg
+ #include <image_transport/image_transport.h>
+ #include <cv_bridge/cv_bridge.h>
+ #include <sensor_msgs/image_encodings.h>
+ #include <geometry_msgs/Twist.h>
+ #include <std_srvs/Empty.h>
+ #include <std_msgs/String.h>
+ #include <sensor_msgs/LaserScan.h>
+ #include <vector>
+ #include <camera_pkg/Coordinate.h>
+ #define IMG_HEIGHT (240)
+ #define IMG_WIDTH (320)
+ #define NUM_THREADS 4
+ #define RANGE_MAX 5.6; 
+ using namespace cv;
+ using namespce std;
+// OpenCV Window Name
+static const std::string OPENCV_WINDOW = "Image window";
+
+
+// Topics
+static const std::string IMAGE_TOPIC = "/camera/rgb/image_raw";
+
+void image_callback(const sensor_msgs::ImageConstPtr& msg){
+   std_msgs::Header msg_header = msg->header;
+   std::string frame_id = msg_header.frame_id.c_str();
+
+   ROS_INFO_STREAM("New Image from " << frame_id);
+
+   cv_bridge::CvImagePtr cv_ptr;
+
+   try
+   {
+     cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+   }
+   catch (cv_bridge::Exception& e)
+   {
+     ROS_ERROR("cv_bridge exception: %s", e.what());
+     return;
+   }
+    cv::Mat frame = cv_ptr->image;
+    int frame_width = frame.get(cv::CAP_PROP_FRAME_WIDTH);
+    int frame_height = frame.get(cv::CAP_PROP_FRAME_HEIGHT);
+    VideoWriter video("outcpp.avi", cv::VideoWriter::fourcc('M','J','P','G'), 10, Size(frame_width,frame_height));
+    video.write(frame);
+
+    cv::imshow("original", frame);
+    cv::waitKey(3);
+
+   int main(int argc, char** argv)
+ {
+    ros::init(argc, argv, "roscpp_example");
+    ros::NodeHandle nh;
+    ros::Subscriber image;
+    nh.image.subscribe(IMAGE_TOPIC, 1000, image_callback);
+    ros::spin();
+    destroyAllWindows();
+ }
