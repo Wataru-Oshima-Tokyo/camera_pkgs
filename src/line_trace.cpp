b892da730033fe0b64e6fcdf526c25a5e2f8cdd2
walter0514 <62457768+Wataru-Oshima-Tokyo@users.noreply.github.com>
2022-04-14 17:45:03 -0700

    Update line_trace.cpp

--- src/line_trace.cpp
+++ src/line_trace.cpp
@@ -179,11 +179,11 @@ void LINETRACE::scan_callnack(const sensor_msgs::LaserScan::ConstPtr& msg)
             min2 = *sm2;
             min3 = *sm3;
             min4 = *sm4;
-            std::cout << "here" <<std::endl;
             angles << " left front: " << min1
             << "// " << "left back: " << min2
             << "// " << "right back: " << min3
             << "// " << "right front: " << min4;
+            std::cout << "here" <<std::endl;
             msg_data.data = angles.str();
             message_pub.publish(msg_data);
         }
