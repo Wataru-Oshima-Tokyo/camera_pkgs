22c64ab3eb1e2cbdb67f9e6338157c442a315d17
Wataru <wataruoshima@my.smccd.edu>
2022-04-15 18:00:09 -0700

    changed videowriter

--- src/record_video_ros.cpp
+++ src/record_video_ros.cpp
@@ -4,6 +4,7 @@
  #include <opencv2/imgproc/imgproc.hpp>
  #include <opencv2/highgui/highgui.hpp>
  #include <opencv2/core/core.hpp>
+ #include <opencv2/videoio.hpp>
 //  #include <opencv2/core/types.hpp>
 
  // Include CvBridge, Image Transport, Image msg
