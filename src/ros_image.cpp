3c716de415354f029d319660627873bacfb206f5
walter0514 <62457768+Wataru-Oshima-Tokyo@users.noreply.github.com>
2022-04-20 10:11:38 -0700

    Update ros_image.cpp

--- src/ros_image.cpp
+++ src/ros_image.cpp
@@ -14,7 +14,7 @@
 using namespace cv;
 
 static const std::string IMAGE_TOPIC = "/camera/rgb/image_raw";
-Mat src, src_hsv;
+Mat src, src_hsv. dst;
 
 void image_callback(const sensor_msgs::ImageConstPtr& msg){
     std_msgs::Header msg_header = msg->header;
