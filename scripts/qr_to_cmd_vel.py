2ac589fd4d960acae8ff5eb6e11de889571a7ac9
Wataru <wataruoshima@my.smccd.edu>
2022-05-10 22:59:45 -0700

    added twist

--- scripts/qr_to_cmd_vel.py
+++ scripts/qr_to_cmd_vel.py
@@ -8,10 +8,15 @@ from std_srvs.srv import Empty, EmptyResponse
 
 def qr_callback(msg):
     print(msg.pose.position.x)
-
+    cmd_pub =rospy.Publisher('cmd_vel', Twist, queue_size=1)
+    twt = Twist()
+    twt.linear.x = msg.pose.position.x
+    twt.linear.y = msg.pose.position.y
+    twt.angualar.z = msg.orientation.z
+    cmd_pub.publish(twt)
 
 if __name__ == "__main__":
     rospy.init_node("QR_conveter")
-    rospy.Publisher('cmd_vel', Twist, queue_size=1)
+    
     rospy.Suscriber("/visp_auto_tracker/object_position", PoseStamped, qr_callback)
     rospy.spin()
