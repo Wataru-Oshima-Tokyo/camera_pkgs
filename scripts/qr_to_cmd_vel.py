d22f91e348301bf9a37173fe20d0cc5c9e1f0d80
Wataru <wataruoshima@my.smccd.edu>
2022-05-17 21:37:59 -0700

    aded a  class

--- scripts/qr_to_cmd_vel.py
+++ scripts/qr_to_cmd_vel.py
@@ -7,44 +7,48 @@ from std_msgs.msg import String, Int8
 from geometry_msgs.msg import Twist, PoseStamped
 from std_srvs.srv import Empty, EmptyResponse
 
-RUN = False
-
-def qr_status_callback(msg):
-    data = msg.data
-    if msg.data == 1:
+class QR_TRACK:
+    def __init__(self):
         RUN = False
-    else:
-        RUN = True
+        rospy.Subscriber("/visp_auto_tracker/object_position", PoseStamped, self.qr_callback)
+        rospy.Subscriber("/visp_auto_tracker/status", Int8, self.qr_status_callback)
+        cmd_pub =rospy.Publisher('cmd_vel', Twist, queue_size=1)
+        twt = Twist()
+    def qr_status_callback(self, msg):
+        if msg.data == 1:
+            self.RUN = False
+        else:
+            self.RUN = True
 
 
-def qr_callback(msg):
-    # print(msg.pose.position.x)
-    cmd_pub =rospy.Publisher('cmd_vel', Twist, queue_size=1)
-    twt = Twist()
-    # twt.linear.x = msg.pose.position.x
+    def qr_callback(self, msg):
+        # print(msg.pose.position.x)
+        
+        
+        # twt.linear.x = msg.pose.position.x
 
-    """
-    Pose Stamped info
-    psoe.position.x is the horizontal position
-    pose.position.z is the distance from robot 0.1 is 10cm
-    """
-    
-    if msg.pose.position.x > 0.05:
-    #    twt.angular.z = - 10 * msg.pose.position.x
-        twt.angular.z = -0.1
-    elif msg.pose.position.x < -0.05:
-    #    twt.angular.z = -10 * msg.pose.position.x
-       twt.angular.z = 0.1
-    else:
-        twt.angular.z = 0
-    # twt.angular.z = msg.pose.position.x
-    # twt.angular.z = -msg.pose.orientation.z
-    # if RUN:
-    #     cmd_pub.publish(twt)
-    cmd_pub.publish(twt)
+        """
+        Pose Stamped info
+        psoe.position.x is the horizontal position
+        pose.position.z is the distance from robot 0.1 is 10cm
+        """
+        
+        if msg.pose.position.x > 0.05:
+            self.twt.angular.z = - 10 * msg.pose.position.x
+            # twt.angular.z = -0.1
+        elif msg.pose.position.x < -0.05:
+            self.twt.angular.z = -10 * msg.pose.position.x
+            # twt.angular.z = 0.1
+        else:
+            self.twt.angular.z = 0
+        # twt.angular.z = msg.pose.position.x
+        # twt.angular.z = -msg.pose.orientation.z
+        if self.RUN:
+            self.cmd_pub.publish(self.twt)
+
 if __name__ == "__main__":
     rospy.init_node("QR_conveter")
-    rospy.Subscriber("/visp_auto_tracker/object_position", PoseStamped, qr_callback)
-    rospy.Subscriber("/visp_auto_tracker/status", Int8, qr_status_callback)
+    
+    
     rospy.spin()
 
