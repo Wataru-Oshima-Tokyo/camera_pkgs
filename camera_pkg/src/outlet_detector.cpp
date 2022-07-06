7c87f51e223de4b35a5bf5f136f98b4cfbdc2549
DC Studio03（Email） <dc_studio03@techshare.co.jp>
2022-07-06 11:59:46 +0900

    added outlet_detector

--- camera_pkg/src/outlet_detector.cpp
+++ camera_pkg/src/outlet_detector.cpp
@@ -305,7 +305,7 @@ int main( int argc, char** argv )
         clock_gettime(CLOCK_MONOTONIC, &stop); fstop=(double)stop.tv_sec + ((double)stop.tv_nsec/1000000000.0);
         std::string fps= "FPS: " + std::to_string(1/(fstop-fstart));
         std::string exp=""
-        if(cc.mode =="L"){
+        if(cc.mode =="L" || cc.cmd == "R"){
             if(cc.RUN)
                 exp ="Get Coordinate";
             else 
