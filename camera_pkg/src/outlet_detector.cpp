7af5b6a9083511ae10d93a8ef967f32bbf00518f
DC Studio03（Email） <dc_studio03@techshare.co.jp>
2022-07-11 15:35:17 +0900

    added

--- camera_pkg/src/outlet_detector.cpp
+++ camera_pkg/src/outlet_detector.cpp
@@ -369,6 +369,7 @@ int main( int argc, char** argv )
             cv::namedWindow(cc.ROI_WINDOW,WINDOW_AUTOSIZE);
             cv::setMouseCallback(cc.ROI_WINDOW, get_hsv,&cc);
             imshow(cc.ROI_WINDOW, cc.ROI);
+            imshow("hsv", cc.src_hsv);
           }
       waitKey(3);      
       }
