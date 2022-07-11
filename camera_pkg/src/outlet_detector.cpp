ac3d6b2b50d96e24911c2d2a2780e2e9a05d94ca
walter0514 <62457768+Wataru-Oshima-Tokyo@users.noreply.github.com>
2022-07-11 14:37:21 +0900

    Update outlet_detector.cpp

--- camera_pkg/src/outlet_detector.cpp
+++ camera_pkg/src/outlet_detector.cpp
@@ -353,19 +353,18 @@ int main( int argc, char** argv )
       // cout << cc.getRun() << endl;
       clock_gettime(CLOCK_MONOTONIC, &start); fstart=(double)start.tv_sec + ((double)start.tv_nsec/1000000000.0);
       if(!cc.src.empty()){
-          while(!cc.Drew){
+	if(!cc.Drew){
 	    imshow(cc.SRC_WINDOW, cc.src);
-	    waitKey(3);
-	  }
-	      destroyAllWindows();
+	}else{
             cv::rectangle(cc.src, cv::Point(cc.ix,cc.iy),  cv::Point(cc.cx,cc.cy), cv::Scalar(0,255,255), 2,4);
             //make the region of interest
             cc.makeRegion(0, 0);
             imshow("ROI", cc.ROI);
             cv::namedWindow(cc.HSV_WINDOW,WINDOW_AUTOSIZE);
             cv::setMouseCallback(cc.HSV_WINDOW, get_hsv);
-	    waitKey(3);
+	    
       }
+      waitKey(3);      
       loop_rate.sleep();
       ros::spinOnce();
       
