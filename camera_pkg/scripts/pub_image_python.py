eff8697b6e90c6effbf78db3af46d8923110aeb4
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-07-22 17:41:35 +0900

    added pub_image_python

---
+++ camera_pkg/scripts/pub_image_python.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import Image
+
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+import time
+
+rospy.init_node('VideoPublisher', anonymous=True)
+
+VideoRaw = rospy.Publisher('VideoRaw', Image, queue_size=10)
+
+cam = cv2.VideoCapture(0)
+if cam.isOpened(): 
+    w  = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH)) # float `width`
+    h = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))  # float `height`
+    fps = cam.get(cv2.CAP_PROP_FPS)
+
+while cam.isOpened():
+    meta, frame = cam.read()
+    frame = frame.resize(src, (w, h))
+    # I want to publish the Canny Edge Image and the original Image
+    msg_frame = CvBridge().cv2_to_imgmsg(frame)
+
+    VideoRaw.publish(msg_frame, "RGB8")
+
+    time.sleep(0.1)
