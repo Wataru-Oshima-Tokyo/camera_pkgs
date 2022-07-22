b728681ea5c0cca0ce433e990dc0b19d2e368d00
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-07-22 18:08:38 +0900

    added pub_image_python

--- camera_pkg/scripts/pub_image_python.py
+++ camera_pkg/scripts/pub_image_python.py
@@ -23,10 +23,10 @@ if __name__ == "__main__":
     while not rospy.is_shutdown():
         meta, frame = cam.read()
         frame = cv2.resize(frame, (w/2, h/2))
-        cv2.imshow("frame", frame)
+        # cv2.imshow("frame", frame)
         # I want to publish the Canny Edge Image and the original Image
         msg_frame =bridge.cv2_to_imgmsg(frame, encoding="passthrough")
 
         VideoRaw.publish(msg_frame)
-        cv2.waitKey(3)
+        # cv2.waitKey(3)
         rate.sleep()
