9c187b9d63a87ea7eaf42bbec061aa97d07c3631
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-07-25 16:02:55 +0900

    addded adjustment control

--- camera_pkg/src/outlet_detector.cpp
+++ camera_pkg/src/outlet_detector.cpp
@@ -192,7 +192,7 @@ void OUTLET_CV::get_circle(int, void*userdata){
         twist.linear.z = move_y;
       }
       
-      if (!mg400_running && (fstop-fstart)>timer && (!Done_x || !Done_y)){
+      if (!mg400_running && (fstop-fstart)>timer && (!Done_x || !Done_y){
         clock_gettime(CLOCK_MONOTONIC, &timer_start); fstart=(double)timer_start.tv_sec + ((double)timer_start.tv_nsec/1000000000.0);
         printf("\nOffset_x: %lf, Offset_y: %lf\n", offset_x, offset_y);
         printf("\nlinear.y: %lf, linear.z: %lf\n", twist.linear.y, twist.linear.z);
