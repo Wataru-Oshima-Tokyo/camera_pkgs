bf37358e0bfa9e88f14f73b3316373117682ed45
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-07-25 15:07:36 +0900

    addded adjustment control

--- camera_pkg/src/outlet_detector.cpp
+++ camera_pkg/src/outlet_detector.cpp
@@ -177,7 +177,7 @@ void OUTLET_CV::get_circle(int, void*userdata){
 
       //比率ゲイン
       double Kp = 0.1;
-      double Kv = 0.0;
+      double Kv = 10.0;
       //PD control
       double move_x = Kp*offset_x - Kv*offset_x/1000;
       double move_y = Kp*offset_y - Kv*offset_y/1000;
