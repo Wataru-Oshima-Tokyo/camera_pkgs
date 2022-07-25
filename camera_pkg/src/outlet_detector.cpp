b56d225698c1235c85f580ef8efde893c73b70d2
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-07-25 13:40:25 +0900

    added publish

--- camera_pkg/src/outlet_detector.cpp
+++ camera_pkg/src/outlet_detector.cpp
@@ -176,8 +176,7 @@ void OUTLET_CV::get_circle(int, void*userdata){
       //PD control
       double move_x = Kp*offset_x - Kv*offset_x/1000;
       double move_y = Kp*offset_y - Kv*offset_y/1000;
-      // double move_x = -(Kp*offset_x - Kv*offset_x/1000);
-      // double move_y = Kp*offset_y - Kv*offset_y/1000;
+
       if(std::abs(offset_x)<0.5){
         twist.linear.y = 0;
       }else{
