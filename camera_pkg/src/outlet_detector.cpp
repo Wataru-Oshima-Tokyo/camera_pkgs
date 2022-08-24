9428cf946a73e98b96724bd687f47cf4d0d51148
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-08-24 11:25:32 +0900

    added a comment

--- camera_pkg/src/outlet_detector.cpp
+++ camera_pkg/src/outlet_detector.cpp
@@ -206,7 +206,7 @@ void OUTLET_CV::get_circle(int, void*userdata){
         // printf("\nlinear.y: %lf, linear.z: %lf\n", twist.linear.y, twist.linear.z);
         ROS_INFO("\nOffset_x: %lf, Offset_y: %lf\n", offset_x, offset_y);
         ROS_INFO("\nlinear.y: %lf, linear.z: %lf\n", twist.linear.y, twist.linear.z);
-        // cmd_vel_pub.publish(twist);
+        cmd_vel_pub.publish(twist);
         if(std::abs(offset_x)<=0.5){
              Done_x = true;
         }
@@ -220,7 +220,7 @@ void OUTLET_CV::get_circle(int, void*userdata){
         coordinate.x = 10;
         coordinate.y = 10;
         coordinate.z = 10;
-        // pub.publish(coordinate);
+        pub.publish(coordinate);
       }
         
       
