150bbd3661f5236c9994d900a830a6cecef575c9
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-08-25 11:57:15 +0900

    added a comment

--- camera_pkg/src/aruco_detector.cpp
+++ camera_pkg/src/aruco_detector.cpp
@@ -63,12 +63,17 @@ class Image{
         }
          
         Mat imageCopy;
+        src.copyTo(imageCopy);
         std::vector<int> ids;
         std::vector<std::vector<cv::Point2f> > corners;
         aruco::detectMarkers(src, dictionary, corners, ids);
         // if at least one marker detected
-        if (ids.size() > 0)
-            cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
+        if (ids.size() > 0){
+            ROS_INFO_STREAM("detected");
+            aruco::drawDetectedMarkers(imageCopy, corners, ids);
+        }
+            
+
         imshow("original", src);
         if(!imageCopy.empty())
             imshow("out", imageCopy);
